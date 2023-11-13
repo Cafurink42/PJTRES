de47ad61d7fb99050c9895ee9ee49caf66c43217
Cafurink42 <carlosdanielcotticaguth@gmail.com>
2023-11-13 01:28:22 -0300

    many alterations

--- index.py
+++ index.py
@@ -11,9 +11,21 @@ root = tk.Tk()
 def Btn():
     print ("Oh ! Do you click on the button !!!")
     print("Congratulations !!!")
-btn  = tk.Button (root, text = 'Click Here', bg = 'green', fg = 'black', command=Btn ) 
+    print (btn)
+def Btn_2():
+    print (btn_2)
+def result():
+    resultado = btn + btn_2
+    print ('O resultado é {}:'.format(resultado))
 
-
+btn  = tk.Button (root, text = '1', bg = 'green', fg = 'black', command=Btn ) 
 btn.place(x= 200, y = 200)
+btn_2  = tk.Button (root,  text = '2', bg ='red', fg = 'black' , command = Btn_2) 
+btn_2.place(x = 100, y = 200)
+result  = tk.Button(root, text = '=', bg ='blue', fg ='black', command = result )
+result.place(x = 300, y = 200)
+btn = 1
+btn_2 = 2
+
 root.geometry('500x500')
 root.mainloop()
