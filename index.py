27a8db90d79c2bd2f67639d51d5d39dbeae4bfe7
Cafurink42 <carlosdanielcotticaguth@gmail.com>
2023-11-14 00:12:19 -0300

    better and better

--- index.py
+++ index.py
@@ -8,22 +8,21 @@ else:
 
 root = tk.Tk()
 
-def btn():
-    if answer_one:
+def btn(button):
+    if button == "Pizza":
         output.config(text = 'Você não é nada saudável coleguinha !')
 
-def btnBad():
-
-    if answer_two:
+    elif button ==  "Abobora":
         output.config(text = 'Você é muito saudável coleguinha ! Parabéns !!!')
 
 
 question = tk.Label(root, text = 'O que você mais gosta de comer ?' , bg = 'blue')
 question.pack()
 
-answer_one = tk.Button(root, text  ='Pizza', bg ='red', command=btn)
+answer_one = tk.Button(root, text  ='Pizza', bg ='red', command=lambda: btn("Pizza"))
 answer_one.pack()
-answer_two = tk.Button(root, text  ='Abobora', bg = 'orange', command = btnBad)
+answer_two = tk.Button(root, text  ='Abobora', bg = 'orange', command=lambda:btn("Abobora")) #lambda passa argumento para a função
+                                                                                             #quando for chamado o botão
 answer_two.pack()
 
 output = tk.Label(root, text = '', bg = 'white' , width=100, height=40)
