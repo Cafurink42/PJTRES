38d8c7ea6c31785cf3c3788515c6c1bc1b54e3c6
Cafurink42 <carlosdanielcotticaguth@gmail.com>
2023-11-13 23:58:59 -0300

    How is going your health ?

--- index.py
+++ index.py
@@ -4,28 +4,33 @@ if sys.version_info[0] == 3:
     import tkinter as tk
 else:
     import Tkinter as tk 
-
+  
 
 root = tk.Tk()
 
-def Btn():
-    print ("Oh ! Do you click on the button !!!")
-    print("Congratulations !!!")
-    print (btn)
-def Btn_2():
-    print (btn_2)
-def result():
-    resultado = btn + btn_2
-    print ('O resultado é {}:'.format(resultado))
+def btn():
+    if answer_one:
+        output.config(text = 'Você não é nada saudável coleguinha !')
 
-btn  = tk.Button (root, text = '1', bg = 'green', fg = 'black', command=Btn ) 
-btn.place(x= 200, y = 200)
-btn_2  = tk.Button (root,  text = '2', bg ='red', fg = 'black' , command = Btn_2) 
-btn_2.place(x = 100, y = 200)
-result  = tk.Button(root, text = '=', bg ='blue', fg ='black', command = result )
-result.place(x = 300, y = 200)
-btn = 1
-btn_2 = 2
+def btnBad():
+
+    if answer_two:
+        output.config(text = 'Você é muito saudável coleguinha ! Parabéns !!!')
+
+
+question = tk.Label(root, text = 'O que você mais gosta de comer ?' , bg = 'blue')
+question.pack()
+
+answer_one = tk.Button(root, text  ='Pizza', bg ='red', command=btn)
+answer_one.pack()
+answer_two = tk.Button(root, text  ='Abobora', bg = 'orange', command = btnBad)
+answer_two.pack()
+
+output = tk.Label(root, text = '', bg = 'white' , width=100, height=40)
+output.pack()
+
+
+
 
 root.geometry('500x500')
 root.mainloop()
