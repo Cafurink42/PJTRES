f9a74f2a5793e58896289fa65cd52690e2f40b9d
Cafurink42 <carlosdanielcotticaguth@gmail.com>
2023-12-09 23:19:55 -0300

    Bad Lucky or Very Lucky with tk interface test

--- index2.py
+++ index2.py
@@ -1,10 +1,39 @@
+
 from random import * 
 
-typeAnumber = int(input('Digite um número de 0 a 1:'))
+import sys 
 
-randomNum = round(random())
-
-if (typeAnumber == randomNum):
-    print ('Você deu sorte')
+if sys.version_info[0] == 3:
+    import tkinter as tk
 else:
-    print ('Você deu azar')
+    import Tkinter as tk 
+
+
+root = tk.Tk()
+
+def btn():
+    getInsert = int (InserNum.get())
+    randomNum = round(random())
+    print(randomNum)
+    if getInsert == randomNum:
+        output.config(text = 'Very Lucky ! ')
+    else:
+        output.config(text = 'Bad Lucky :( ')
+        
+
+title = tk.Label(root, text = 'Try the Lucky ')
+title.place(x = 170, y = 0)
+InserNum = tk.Entry(root, bg = 'gray')
+InserNum.place(x = 170, y = 20)
+bt = tk.Button(root, text = 'Send', command=btn)
+bt.place(x = 170, y  = 70)
+output  = tk.Label(root, text = '', width= 50, height= 10, bg  = 'green')
+output.place(x = 50, y = 120)
+
+
+
+
+
+root.title('Bad or Lucky')
+root.geometry('500x450')
+root.mainloop()
