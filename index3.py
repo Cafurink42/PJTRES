5209c0d8b9a1751b04de3aeed66fa54bf699286e
Cafurink42 <carlosdanielcotticaguth@gmail.com>
2023-12-11 17:16:49 -0300

    Add teams in list

---
+++ index3.py
@@ -0,0 +1,44 @@
+from random import * 
+import tkinter as tk  
+
+root  = tk.Tk()
+
+def Teams():
+
+    if len(player_list) < maxplayers:
+            getName = (PlayeresNames.get())
+            player_list.append(getName)
+            output.config(text  = '{}'.format(player_list))
+
+
+def SortTeams():
+      shuffle(player_list)
+      output.config(text = 'Time sorteado {}'.format(player_list))
+
+
+
+
+title = tk.Label(root, text  = 'Insert your team')
+title.pack()
+PlayeresNames = tk.Entry(root, bg = 'gray')
+PlayeresNames.pack()
+FormTeams =  tk.Button(root, text = 'Send', command  = Teams)
+FormTeams.pack()
+Sortteam = tk.Button(root, text = 'Sort Team', command  = SortTeams)
+Sortteam.pack()
+
+
+output = tk.Label(root, bg = 'yellow', width= 50, height=10)
+output.pack()
+
+player_list = []
+maxplayers  = 5
+
+
+
+
+
+root.title('Add Teams')
+root.geometry('500x600')
+root.config(background= 'green')
+root.mainloop()
